39829c117a571d90bbe6844a6aa2ce98040d3333
Andrey Yankin <yankin013@gmail.com>
2013-01-24 22:39:18 +0400

    411 faster

--- Problem411.hs
+++ Problem411.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BangPatterns, TemplateHaskell, TypeFamilies, MultiParamTypeClasses #-}
 
 module Main where
 
@@ -10,88 +10,102 @@ import Control.Monad.Primitive
 import System.Environment
 import Data.List
 import Data.Set (toList, fromList)
-import qualified Data.Vector as V
-import qualified Data.Vector.Mutable as MV
---import qualified Data.Vector.Algorithms.Intro as AI
+import qualified Data.Vector.Generic
+import qualified Data.Vector.Generic.Mutable
+import Data.Vector.Unboxed.Deriving
+import qualified Data.Vector.Unboxed as V
+import Data.Vector.Unboxed (Unbox)
+import qualified Data.Vector.Unboxed.Mutable as MV
 
---import qualified Graphics.Gnuplot.Simple as GP
+data Position a = Position {-# UNPACK #-} !a {-# UNPACK #-} !a
+  deriving (Eq, Ord, Show)
+
+derivingUnbox "Position"
+    [t| (Unbox a) => Position a -> (a, a) |]
+    [| \ (Position x y) -> (x, y) |]
+    [| \ (x, y) -> Position x y |]
+
+data Station a = Station {-# UNPACK #-} !(Position a) {-# UNPACK #-} !Int
+  deriving (Eq, Ord, Show)
+
+derivingUnbox "Station"
+    [t| (Unbox a) => Station a -> (Position a, Int) |]
+    [| \ (Station x y) -> (x, y) |]
+    [| \ (x, y) -> Station x y |]
+
+data Distance a = Distance {-# UNPACK #-} !Int {-# UNPACK #-} !(Position a)
+  deriving (Eq, Ord, Show)
+
+derivingUnbox "Distance"
+    [t| (Unbox a) => Distance a -> (Int, Position a) |]
+    [| \ (Distance x y) -> (x, y) |]
+    [| \ (x, y) -> Distance x y |]
+
+
+distance (Distance dist _) = dist
+{-# INLINE distance #-}
 
 xs :: Int -> [Int]
 xs n = xs'
  where xs' = 1 : map (\x -> 2*x `mod` n) xs'
+{-# INLINE xs #-}
 
 ys :: Int -> [Int]
 ys n = ys'
  where ys' = 1 : map (\y -> 3*y `mod` n) ys'
+{-# INLINE ys #-}
 
-stations :: Int -> [(Int, Int)]
-stations n = reverse . toList . fromList . ((0,0) :) . take (2*n+1) $ zip (xs n) (ys n)
+stations :: Int -> [Position Int]
+stations n = reverse . toList . fromList . ((Position 0 0) :) . take (2*n+1) $ zipWith Position (xs n) (ys n)
+{-# INLINE stations #-}
 
-process :: (PrimMonad m) => MV.MVector (PrimState m) (Int, (Int, Int)) -> ((Int, Int), Int) -> m ()
-process v (station, l) = do
-  --v' <- MV.unsafeGrow v 1
-  --ss <- V.unsafeFreeze v
-  max <- maximum $ l - 1
-  let current = (max, station)
-  MV.unsafeWrite v l current
+process :: (PrimMonad m) => MV.MVector (PrimState m) (Distance Int) -> Station Int -> m ()
+process v s@(Station pos l) = do
+  max <- findMax v s $ l - 1
+  let current = Distance max pos
+  MV.unsafeWrite v l $! current
   place v current l
-  --AI.sortBy (flip compare) $ MV.unsafeDrop l v
-  --return v
- where --maximum s = V.foldl' (\max (curr, x) -> if (higherThan s x && curr > max) then curr else max) (-1)
-       maximum !i = do
-         if i < 0
-         then return 0
-         else do
-           (curr, x) <- MV.unsafeRead v i
-           if higherThan station x
-           then return $ curr + 1
-           else maximum $ i - 1
-       n = MV.length v
-       {-maximum = do
-         foldM (\max i -> do
-           (curr, x) <- MV.unsafeRead v i
-           return $ if (higherThan station x && curr > max) then curr else max) (-1) [0..l-1]-}
+{-# INLINE process #-}
 
-place !v !max@(val1,_) !i = place' i
+findMax v (Station !pos _) i = findMax' i
+ where
+  findMax' !i = do
+    if i < 0
+    then return 0
+    else do
+      (Distance curr x) <- MV.unsafeRead v i
+      if higherThan pos x
+      then return $ curr + 1
+      else findMax' $ i - 1
+{-# INLINE findMax #-}
+
+place :: (PrimMonad m) =>
+     MV.MVector (PrimState m) (Distance Int) -> Distance Int -> Int -> m ()
+place v max@(Distance !val1 _) i = place' i
  where 
   place' !i = do
     let j = i - 1
     if j < 0
     then return ()
     else do
-      !curr@(!val2, _) <- MV.unsafeRead v j
+      curr@(Distance val2 _) <- MV.unsafeRead v j
       if val2 > val1
       then do
-        MV.unsafeWrite v j max
-        MV.unsafeWrite v i curr
+        MV.unsafeWrite v j $! max
+        MV.unsafeWrite v i $! curr
         place' j
       else return ()
   n = MV.length v
 {-# INLINE place #-}
 
-{-
-genLengths :: V.Vector (Int, Int) -> V.Vector (Int, (Int, Int))
-genLengths = V.foldl' process V.empty
- where
-  process ss s =
-    let
-        length :: Int
-        length = V.foldl' (\max (curr, x) -> if (higherThan s x && curr > max) then curr else max) (-1) ss
-        new = runST $ do
-                v  <- V.unsafeThaw ss
-                v' <- MV.unsafeGrow v 1
-                MV.write v' (V.length ss) (length+1, s)
-                V.unsafeFreeze v'
-    in new
--}
-higherThan (x1, y1) (x2, y2) = (x2 >= x1 && y2 >= y1) --- || (x2 > x1 && y2 >= y1)
+higherThan (Position x1 y1) (Position x2 y2) = (x2 >= x1 && y2 >= y1) --- || (x2 > x1 && y2 >= y1)
 {-# INLINE higherThan #-}
 
 lengths n = runST $ do
                   let ss = stations n
                   let l = length ss
-                  v <- MV.replicate l (0, (0, 0))
-                  mapM_ (process v) $ zip ss $ [0..l-1]
+                  v <- MV.replicate l $ Distance 0 $ Position 0 0
+                  mapM_ (process v) $ zipWith Station ss $ [0..l-1]
                   V.unsafeFreeze v
 
 -- |
@@ -106,9 +120,7 @@ lengths n = runST $ do
 -- 48
 -- 
 maxPathLength :: Int -> Int
-maxPathLength = fst . V.last . lengths
-
---plotStations = GP.plotPathStyle [GP.Key Nothing] (GP.PlotStyle GP.Points $ GP.CustomStyle [GP.PointSize 5]) . map snd . stations
+maxPathLength = distance . V.last . lengths
 
 main = do
   args <- getArgs
