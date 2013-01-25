1edc01f440036fe62bdc29dc06fa3b6346140c8a
Andrey Yankin <yankin013@gmail.com>
2013-01-26 00:58:54 +0400

    tried unboxed-containers

--- Problem411.hs
+++ Problem411.hs
@@ -5,77 +5,76 @@ module Main where
 import Control.Parallel.Strategies
 import Control.Parallel
 
-import Control.Monad
-import Control.Monad.ST
-import Control.Monad.Primitive
-
 import System.Environment
 import Data.List
---import qualified Data.Set as S
+import qualified Data.Set.Unboxed as S
 import qualified Data.IntMap.Strict as M
-
-import qualified Data.Vector.Generic
-import qualified Data.Vector.Generic.Mutable
-import Data.Vector.Unboxed.Deriving
-import qualified Data.Vector.Unboxed as V
-import Data.Vector.Unboxed (Unbox)
-import qualified Data.Vector.Unboxed.Mutable as MV
-
-import qualified Data.Vector.Algorithms.Intro as H
 
 data Position = Position {-# UNPACK #-} !Int {-# UNPACK #-} !Int
   deriving (Eq, Ord, Show)
 
-derivingUnbox "Position"
-  [t| Position -> (Int, Int) |]
-  [| \ (Position x y) -> (x, y) |]
-  [| \ (x, y) -> Position x y |]
+instance S.US Position where
+    data USet Position = PositionTip | PositionBin {-# UNPACK #-} !S.Size {-# UNPACK #-} !Int{-# UNPACK #-} !Int !(S.USet Position) !(S.USet Position)
+    view PositionTip = S.Tip
+    view (PositionBin s a b l r) = S.Bin s (Position a b) l r
+    tip = PositionTip
+    bin s (Position a b) = PositionBin s a b
 
 getY (Position _ y) = y
 {-# INLINE getY #-}
 
-stationsList = iterate (\(Position x y) -> Position (2*x `mod` n) (3*y `mod` n)) (Position 1 1)
+stationsList n = iterate (\(Position x y) -> Position (2*x `mod` n) (3*y `mod` n)) (Position 1 1)
+{-
+fillSetWithoutCycles = fillSet' S.empty 0
+ where
+  fillSet' set _       []     = set
+  fillSet' set oldSize (x:xs) =
+    let newSet = S.insert x set
+        newSize = S.size newSet
+    in if oldSize == newSize
+       then set
+       else fillSet' newSet newSize xs
 
-stations :: Int -> V.Vector Position
-stations n = V.iterateN (2*n+1) (\(Position x y) -> Position (2*x `mod` n) (3*y `mod` n)) (Position 1 1)
+
+fromList :: [Position] -> S.Set Position
+fromList [] = S.empty
+fromList [x] = S.singleton x
+fromList (x0 : xs0) = fromList' (S.singleton x0) xs0
+  where
+    fromList' t0 xs = foldlStrict ins t0 1 xs
+      where ins t x = S.insert x t
+{-# INLINE fromList #-}
+
+foldlStrict :: (S.Set Position -> Position -> S.Set Position) -> S.Set Position -> Int -> [Position] -> S.Set Position
+foldlStrict f = go
+ where
+  go z _       []     = z
+  go z oldSize (x:xs) = 
+    let z' = f z x
+        newSize = S.size z'
+    in if oldSize == newSize
+       then z
+       else z' `seq` go z' newSize xs
+{-# INLINE foldlStrict #-}
+-}
+stations :: Int -> S.USet Position
+stations n = S.insert (Position 0 0) . S.fromList . take (2*n+1) . stationsList $ n
 {-# INLINE stations #-}
 
-process :: M.IntMap Int -> Position -> M.IntMap Int
-process m (Position _ y) =
-  let Just (_, max) = M.lookupLE y m
+process :: Position -> M.IntMap Int -> M.IntMap Int
+process (Position _ y) m =
+  let Just (_, max) = M.lookupGE y m
       newVal = max+1
       m' = M.insert y newVal m
-      m'' = case M.lookupGT y m of
+      m'' = case M.lookupLT y m of
               Just (k, v) -> if v>newVal then m' else M.delete k m'
               Nothing     -> m'
   in m''
 {-# INLINE process #-}
 
-unsafeUnique :: (PrimMonad m) =>
-  MV.MVector (PrimState m) Position -> Int -> m (MV.MVector (PrimState m) Position)
-unsafeUnique v l = do
-  initial <- MV.unsafeRead v 0
-  unsafeUnique' 1 1 initial
- where
-  unsafeUnique' !c !i !last | i == l = return $ MV.unsafeTake c v
-  unsafeUnique' !c !i !last = do
-    current <- MV.unsafeRead v i
-    if current == last
-    then unsafeUnique' c (i+1) last
-    else do
-      MV.unsafeWrite v c $! current
-      unsafeUnique' (c+1) (i+1) current
-
 lengths n =
-  let m = M.singleton 0 (-1) 
-  in V.foldl' process m $ runST $ do
-                  let ss = stations n
-                      l  = V.length ss
-                  v  <- V.unsafeThaw ss
-                  v' <- MV.unsafeGrow v 1
-                  MV.unsafeWrite v' l (Position 0 0)
-                  H.sort v'
-                  unsafeUnique v' (l+1) >>= V.unsafeFreeze
+  let m = M.singleton n (-1) 
+  in S.fold process m (stations n)
 
 -- |
 -- 
@@ -89,10 +88,10 @@ lengths n =
 -- 48
 -- 
 maxPathLength :: Int -> Int
-maxPathLength = snd . M.findMax . lengths
+maxPathLength = snd . M.findMin . lengths
 
 main = do
   args <- getArgs
   let n = read $ head args
-  print $ sum . parMap rseq maxPathLength . map (^5) $ [1..n]
+  print $ sum . parMap rseq maxPathLength . map (^5) $ [n..n]
 
