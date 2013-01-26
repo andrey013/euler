c1ebcf025ac3c9b23f5fe24a41fd3952840a29dd
Andrey Yankin <yankin013@gmail.com>
2013-01-26 23:21:13 +0400

    411 intmap intset

--- Problem411.hs
+++ Problem411.hs
@@ -7,64 +7,32 @@ import Control.Parallel
 
 import System.Environment
 import Data.List
-import qualified Data.Set.Unboxed as S
+import qualified Data.IntSet as S
 import qualified Data.IntMap.Strict as M
 
-data Position = Position {-# UNPACK #-} !Int {-# UNPACK #-} !Int
-  deriving (Eq, Ord, Show)
+stationsList n = iterate (\(x, y) -> (2*x `mod` n, 3*y `mod` n)) (1, 1)
+{-# INLINE stationsList #-}
 
-instance S.US Position where
-    data USet Position = PositionTip | PositionBin {-# UNPACK #-} !S.Size {-# UNPACK #-} !Int{-# UNPACK #-} !Int !(S.USet Position) !(S.USet Position)
-    view PositionTip = S.Tip
-    view (PositionBin s a b l r) = S.Bin s (Position a b) l r
-    tip = PositionTip
-    bin s (Position a b) = PositionBin s a b
-
-getY (Position _ y) = y
-{-# INLINE getY #-}
-
-stationsList n = iterate (\(Position x y) -> Position (2*x `mod` n) (3*y `mod` n)) (Position 1 1)
-
-fillSetWithoutCycles n = fillSet' S.empty 0
+fillMapWithoutCycles = flip fillMap' [(0, 0)] . fillMap' M.empty
  where
-  fillSet' set _       []     = set
-  fillSet' set oldSize (x@(Position a b):xs)
-    | abs (a-b) > (n `div` 4) + 1 = fillSet' set oldSize xs
-    | otherwise =
-    let newSet = S.insert x set
+  fillMap' map []     = map
+  fillMap' map ((!a, !b):ss) =
+    let (!newSet, oldSize) = case M.lookup a map of
+                              Just oldSet -> (S.insert b oldSet, S.size oldSet)
+                              Nothing     -> (S.singleton b, 0)
         newSize = S.size newSet
+        newMap = M.insert a newSet map
     in if oldSize == newSize
-       then set
-       else fillSet' newSet newSize xs
-{-
+       then map
+       else fillMap' newMap ss
+{-# INLINE fillMapWithoutCycles #-}
 
-fromList :: [Position] -> S.Set Position
-fromList [] = S.empty
-fromList [x] = S.singleton x
-fromList (x0 : xs0) = fromList' (S.singleton x0) xs0
-  where
-    fromList' t0 xs = foldlStrict ins t0 1 xs
-      where ins t x = S.insert x t
-{-# INLINE fromList #-}
-
-foldlStrict :: (S.Set Position -> Position -> S.Set Position) -> S.Set Position -> Int -> [Position] -> S.Set Position
-foldlStrict f = go
- where
-  go z _       []     = z
-  go z oldSize (x:xs) = 
-    let z' = f z x
-        newSize = S.size z'
-    in if oldSize == newSize
-       then z
-       else z' `seq` go z' newSize xs
-{-# INLINE foldlStrict #-}
--}
-stations :: Int -> S.USet Position
-stations n = S.insert (Position 0 0) . fillSetWithoutCycles n . take (2*n+1) . stationsList $ n
+stations :: Int -> M.IntMap S.IntSet
+stations n = fillMapWithoutCycles . take (2*n+1) . stationsList $ n
 {-# INLINE stations #-}
 
-process :: M.IntMap Int -> Position -> M.IntMap Int
-process m (Position _ y) =
+process :: M.IntMap Int -> Int -> M.IntMap Int
+process m y =
   let Just (_, max) = M.lookupLE y m
       newVal = max+1
       m' = M.insert y newVal m
@@ -75,8 +43,8 @@ process m (Position _ y) =
 {-# INLINE process #-}
 
 lengths n =
-  let m = M.singleton 0 (-1) 
-  in S.foldl' process m (stations n)
+  let map = M.singleton 0 (-1) 
+  in M.foldl' (S.foldl' process) map (stations n)
 
 -- |
 -- 
@@ -95,5 +63,5 @@ maxPathLength = snd . M.findMax . lengths
 main = do
   args <- getArgs
   let n = read $ head args
-  print $ sum . parMap rseq maxPathLength . map (^5) $ [n..n]
-
+  print $ sum . parMap rseq maxPathLength . map (^5) $ [1..n]
+  --print $ stations $ n^5
