ceba2c263846b245c528f53466409af97cc5e61c
Andrey Yankin <yankin013@gmail.com>
2013-01-28 23:41:45 +0400

    another stupid idea

--- Problem411.hs
+++ Problem411.hs
@@ -9,26 +9,58 @@ import System.Environment
 import Data.List
 import qualified Data.IntSet as S
 import qualified Data.IntMap.Strict as M
+import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Algorithms.Intro as VA
 
-stationsList n = iterate (\(x, y) -> (2*x `mod` n, 3*y `mod` n)) (1, 1)
-{-# INLINE stationsList #-}
+xsGen :: Int -> [Int]
+xsGen n = take (2*n+1) $ iterate (\x -> 2*x `mod` n) 1
 
-fillMapWithoutCycles = flip fillMap' [(0, 0)] . fillMap' M.empty
+ysGen :: Int -> [Int]
+ysGen n = take (2*n+1) $ iterate (\y -> 3*y `mod` n) 1
+
+decycle
+  :: [Int] -> (Int, Int)
+decycle proc = fillMap' M.empty 0 proc
  where
-  fillMap' map []     = map
-  fillMap' map ((!a, !b):ss) =
-    let (!newSet, oldSize) = case M.lookup a map of
-                              Just oldSet -> (S.insert b oldSet, S.size oldSet)
-                              Nothing     -> (S.singleton b, 0)
-        newSize = S.size newSet
-        newMap = M.insert a newSet map
-    in if oldSize == newSize
-       then map
-       else fillMap' newMap ss
-{-# INLINE fillMapWithoutCycles #-}
+  fillMap' map c [] = undefined
+  fillMap' map c (x:xs) =
+    let newMap = if c>30 then map else (M.insert x c map)
+    in case M.lookup x map of
+         Just i  -> (i, c - i)
+         Nothing -> fillMap' newMap (c + 1) xs
 
-stations :: Int -> M.IntMap S.IntSet
-stations n = fillMapWithoutCycles . take (2*n+1) . stationsList $ n
+positions n = (M.fromList $ (0, 0):uniques, xsList, V.fromList yy)
+ where
+  (offsetx, lengthx) = decycle $ xsGen n
+  (offsety, lengthy) = decycle $ ysGen n
+  nou = max offsetx offsety
+  uniques = take nou $ zip (xsGen n) (ysGen n)
+  xx = take lengthx $ drop nou $ xsGen n
+  yy = take lengthy $ drop nou $ ysGen n
+  xsList = V.modify (VA.sort) $ V.fromList $ zip xx $ cycle [0..lengthy-1]
+
+stations :: Int -> [S.IntSet]
+stations n = snd $ V.foldl' (\ (map, l) x -> let (newMap, list) = (createSets x map l) in (newMap, list)) (u, []) xs
+ where
+  (u, xs, ys) = positions n
+  interval = V.length xs
+  ysCount = V.length ys
+
+  createSets :: (Int, Int) -> M.IntMap Int -> [S.IntSet] -> (M.IntMap Int, [S.IntSet])
+  createSets xx@(x, i) map acc =
+    case M.lookupGE 0 map of
+      Just (mx, my) -> if mx == x
+                       then (M.delete mx map, (S.insert my $ createSet xx) : acc)
+                       else if mx < x
+                            then createSets xx (M.delete mx map) $ (S.singleton my) : acc
+                            else (map, (createSet xx) : acc)
+      Nothing       -> (map, (createSet xx) : acc)
+
+  createSet (x, i) = S.fromList .
+                     map (V.unsafeIndex ys) .
+                     (i :) .
+                     takeWhile (/= i) $
+                     iterate (\i -> (i + interval) `mod` ysCount) $ (i + interval) `mod` ysCount
 {-# INLINE stations #-}
 
 process :: M.IntMap Int -> Int -> M.IntMap Int
@@ -44,7 +76,7 @@ process m y =
 
 lengths n =
   let map = M.singleton 0 (-1) 
-  in M.foldl' (S.foldl' process) map (stations n)
+  in foldr (flip $ S.foldl' process) map (stations n)
 
 -- |
 -- 
